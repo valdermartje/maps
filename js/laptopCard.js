432a42e6ae9f59cd143d96b8c4ee005dae377476
valdermartje <45938216+valdermartje@users.noreply.github.com>
2020-06-19 19:29:28 +0200

    Add files via upload

--- js/laptopCard.js
+++ js/laptopCard.js
@@ -26,12 +26,7 @@ class Laptop extends React.Component {
 									marginHeight="0"
 									marginWidth="0"
 									src={
-										// TODO: make this URL near by the user location
-										'https://www.mapquest.com/search/results?query=Hotels&queryId=sic:701101&boundingBox=' +
-										(this.props.lat ? this.props.lat : Math.random() * 30 + 1) +
-										',' +
-										(this.props.long ? this.props.long : Math.random() * 30 + 1) +
-										',6.70759350405294,22.47802734375&page=0'
+										"https://www.mapquest.com/search/results?query=Shopping&queryId=sic:565101,651201,566101,531102&boundingBox="+ (this.props.long ? this.props.long : Math.random() * 30 + 1) + "," + (this.props.lat ? this.props.lat : Math.random() * 30 + 1) + ","+ (this.props.long ? this.props.long : Math.random() * 30 + 1) +","+ (this.props.lat ? this.props.lat : Math.random() * 30 + 1) + "&page=0"
 									}
 								/>
 							</div>
